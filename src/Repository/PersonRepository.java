af9ad1688f9207190b036f4c2bd07c7f595e9062
huypd <huypd@10.12.1.185>
2023-10-13 14:30:26 +0700

    Handle File

---
+++ src/Repository/PersonRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Repository;
+
+import DataAcess.PersonDAO;
+
+/**
+ *
+ * @author Xuan Vinh
+ */
+public class PersonRepository implements IPersonRepository{
+    @Override
+    public void findPersonInfo() {
+        PersonDAO.Instance().findPersonInfo();
+    }
+
+    @Override
+    public void copyNewFile() {
+        PersonDAO.Instance().copyNewFile();
+    }
+}
